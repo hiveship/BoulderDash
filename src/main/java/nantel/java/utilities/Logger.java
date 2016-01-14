03ab1b81135055cab6cf2fdfef2a1e8e0c50c6bc
Maelig Nantel <maelig.nantel@gmail.com>
2016-01-14 09:55:22 +0100

    Update Logger.java

--- src/main/java/nantel/java/utilities/Logger.java
+++ src/main/java/nantel/java/utilities/Logger.java
@@ -4,6 +4,7 @@ public interface Logger
 {
 	boolean isErrorEnabled();
 
+	// Remove ?
 	void setErrorEnable(boolean enable);
 
 	void error(CharSequence content);
@@ -12,18 +13,21 @@ public interface Logger
 
 	void warn(CharSequence content);
 
+	// Remove ?
 	void setWarnEnable(boolean enable);
 
 	boolean isInfoEnabled();
 
 	void info(CharSequence content);
 
+	// Remove ?
 	void setInfoEnable(boolean enable);
 
 	boolean isDebugEnabled();
 
 	void debug(CharSequence content);
 
+	// Remove ?
 	void setDebugEnable(boolean enable);
 
 }
